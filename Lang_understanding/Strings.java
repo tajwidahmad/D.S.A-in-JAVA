4f41e0e6bc6b72d13db1be9b734b82833ae9c468
Tajwid Ahmad <tajwidahmad5@gmail.com>
2026-05-06 21:24:22 +0530

    Strings in java 01 commited

---
+++ Lang_understanding/Strings.java
@@ -0,0 +1,16 @@
+
+package Lang_understanding;
+import java.util.*;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String str = new String("tajwid ahmad");
+        System.out.println(str);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter your name.");
+        String name = sc.nextLine();
+        System.out.println("name entered by you is: "+name);
+
+    }
+}
