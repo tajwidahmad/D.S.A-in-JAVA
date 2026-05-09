6b79d8fabb4deffbc1d52e57b6563d1d3dd9b15b
Tajwid Ahmad <tajwidahmad5@gmail.com>
2026-05-09 20:14:46 +0530

    some important collection methods

--- Collection/collection1.java
+++ Collection/collection1.java
@@ -3,15 +3,28 @@ import java.util.*;
 
 public class collection1 {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        System.out.println(list);
-        list.remove(0);
-        System.out.println(list);
-        list.removeAll(list);
-        System.out.println(list);
+        // ArrayList<Integer> list = new ArrayList<>();
+        // list.add(10);
+        // list.add(20);
+        // list.add(30);
+        // System.out.println(list);
+        // list.remove(0);
+        // System.out.println(list);
+        // list.removeAll(list);
+        // System.out.println(list);
+        
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(7);
+        al.add(2);
+        al.add(10);
+        //printing the list
+        System.out.println("before sort"+ al);
+        Collections.sort(al);
+        System.out.println("after sort"+ al);
+        // shallow copy and deep copy in java collections
+        ArrayList<Integer> marks = new ArrayList<>();
+        marks.ensureCapacity(20);
+        System.out.println(marks.isEmpty());
 
 
     }
